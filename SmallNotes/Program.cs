906489247525b995aa41041a84b97972662558b5
Matt Schneeberger <helluvamatt@gmail.com>
2015-03-27 17:09:35 -0500

    Application skeleton

---
+++ SmallNotes/Program.cs
@@ -0,0 +1,69 @@
+﻿using Common.TrayApplication;
+using log4net.Config;
+using Mono.Options;
+using SmallNotes.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmallNotes
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			// Check if this is the only instance of this application
+			if (!SingleInstance.Start())
+			{
+				// This is not the only instance, exit
+				Console.WriteLine(Resources.AnotherInstance);
+				return;
+			}
+
+			// Configure logging
+			BasicConfigurator.Configure();
+
+			// Create the TrayApplicationContext - the controller of the entire application
+			SmallNotesTrayApplicationContext ctxt = new SmallNotesTrayApplicationContext();
+			ctxt.Logger.Info("Initializing...");
+
+			// Options
+			OptionSet options = new OptionSet() {
+				{ "startup", "Do not show the options form.", v => ctxt.ShowOptionsForm = v == null }
+			};
+			options.Parse(args);
+
+			// Initialize the application
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			ctxt.InitializeContext();
+
+			// Run the application
+			ctxt.Logger.Info("Initialized. Starting main loop...");
+			try
+			{
+				Application.Run(ctxt);
+			}
+			catch (Exception ex)
+			{
+				ctxt.Logger.Error("Unexpected exception:", ex);
+				MessageBox.Show(ex.Message, Resources.ProgramTerminated, MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+
+			// Log the shutdown
+			ctxt.Logger.Info("Exiting...");
+
+			// all finished so release the mutex
+			SingleInstance.Stop();
+
+			
+		}
+	}
+}
---
+++ SmallNotes/SmallNotesTrayApplicationContext.cs
@@ -0,0 +1,98 @@
+﻿using Common.TrayApplication;
+using SmallNotes.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmallNotes
+{
+	public class SmallNotesTrayApplicationContext : TrayApplicationContext
+	{
+		/// <summary>
+		/// Cached feed list
+		/// </summary>
+		//public Dictionary<int, Feed> Feeds { get; private set; }
+
+		/// <summary>
+		/// Config database
+		/// </summary>
+		//public Database ConfigDatabase { get; private set; }
+
+		public SmallNotesTrayApplicationContext() : base()
+		{
+			// TODO Initialize variables
+		}
+
+		#region TrayApplicationContext implementation
+
+		protected override void OnInitializeContext()
+		{
+			// TODO Connect databases, load notes, and display note forms, etc...
+		}
+
+		protected override OptionsForm BuildOptionsForm()
+		{
+			SmallNotesOptionsForm form = new SmallNotesOptionsForm();
+			// TODO Bind events, etc...
+			return form;
+		}
+
+		protected override void BuildContextMenu()
+		{
+			// Build the context menu: showOptionsMenuItem
+			ToolStripMenuItem showOptionsMenuItem = new ToolStripMenuItem(Resources.MenuItemManage);
+			showOptionsMenuItem.Click += showOptionsMenuItem_Click;
+
+			// Build the context menu: feedsMenuItem
+			ToolStripMenuItem newNoteMenuItem = new ToolStripMenuItem(Resources.MenuItemNewNote);
+			newNoteMenuItem.Click += newNoteMenuItem_Click;
+
+			// Build the context menu: exitMenuItem
+			ToolStripMenuItem exitMenuItem = new ToolStripMenuItem(Resources.MenuItemExit);
+			exitMenuItem.Click += exitMenuItem_Click;
+
+			// Build contextMenu
+			notifyIcon.ContextMenuStrip.Items.Add(showOptionsMenuItem);
+			notifyIcon.ContextMenuStrip.Items.Add(new ToolStripSeparator());
+			notifyIcon.ContextMenuStrip.Items.Add(newNoteMenuItem);
+			notifyIcon.ContextMenuStrip.Items.Add(new ToolStripSeparator());
+			notifyIcon.ContextMenuStrip.Items.Add(exitMenuItem);
+		}
+
+		protected override string GetApplicationName()
+		{
+			return Resources.AppTitle;
+		}
+
+		protected override Icon GetApplicationIcon()
+		{
+			return new Icon(GetType(), "appicon.ico");
+		}
+
+		#endregion
+
+		#region Event handlers
+
+		private void showOptionsMenuItem_Click(object sender, EventArgs e)
+		{
+			CreateOptionsForm();
+		}
+
+		private void newNoteMenuItem_Click(object sender, EventArgs e)
+		{
+			// TODO Create a new note, show it's form, etc...
+		}
+
+		private void exitMenuItem_Click(object sender, EventArgs e)
+		{
+			ExitThread();
+		}
+
+		#endregion
+	}
+}
